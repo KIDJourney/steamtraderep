6c7dda1230d6e302d33118c0c07fa5a2545036d2
Lonesome George <kingdeadfish@qq.com>
2014-10-21 22:26:36 +0800

    add class folder and search class

--- class/searchclass.php
+++ class/searchclass.php
@@ -3,25 +3,44 @@
 class search
 {
 //private
-    private $list = array("tiebaid","steamid","64weiid","taobaoid","zhifubaomail","zhifubaoid");
-    private $userinput = $_POST["userinput"];
+    private $namelist = array("tiebaid"=>"贴吧ID :","steamid"=>"steam ID :","64weiid"=>"64位ID :","taobaoid"=>"淘宝ID :","zhifubaomail"=>"支付宝信息 :","zhifubaoid"=>"支付宝ID :");
+    private $userinput = '';
+    private $sqlquery = '';
 //public
     public function __construct()
     {
         if (!$_SERVER["REQUEST_METHOD"] == "POST"){
             die("No right to request");
         } else {
-
+            $userinput = $this->Fliter($_POST["userinput"]);
+            $this->sqlquery = "SELECT * FROM trickerlist where 
+                     tiebaid like '%$userinput%' or 
+                     steamid like '%$userinput%' or 
+                     64weiid like '%$userinput%' or
+                     taobaoid like '%$userinput%' or
+                     zhifubaomail like '%$userinput%' or
+                     zhifubaoid like '%$userinput%'";
+            $this->sqlserach();
         }
     }
 
     public function sqlserach()
     {
-        $mysqli = new mysqli(SAE_MYSQL_HOST.":".SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS,SAE_MYSQL_DB);
+        $mysqli = new mysqli(SAE_MYSQL_HOST_M.":".SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS,SAE_MYSQL_DB);
         if ($mysqli->connect_errno){
-            echo "Failed to connect to MySQL: (" . $mysqli->connect_errno , ")" . $mysqli->connect_error;            
+            die("Failed to connect to MySQL: (" . $mysqli->connect_errno . ")" . $mysqli->connect_error);
+        } else {
+            $result = $mysqli->query($this->sqlquery);
+            $row = $result->fetch_assoc();
+            if (!isset($row)){
+                echo "未查询到相关结果。 . <br> 请谨慎交易。";
+            } else {
+                foreach($row as $keys=>$value){
+                    echo $this->namelist[$keys] . " " . $value . "<br>";
+                }
+                echo "<br>" . "此人可能是骗子。.<br>请勿与他交易！。<br> 请将相关信息汇报给吧务！" ;
+            }
         }
-        echo $mysqli->host_info . "\n";
     }
 
     public function Fliter($input)
