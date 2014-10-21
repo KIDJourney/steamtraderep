218a0c93a6aafbce37289de53fcdcff6368fff29
Lonesome George <kingdeadfish@qq.com>
2014-10-21 22:27:44 +0800

    update

--- saemysqltest.php
+++ saemysqltest.php
@@ -3,5 +3,12 @@
     if ($mysqli->connect_errno){
         echo "Failed to connect to MySQL: (" . $mysqli->connect_errno , ")" . $mysqli->connect_error;            
     }
-    echo $mysqli->host_info . "\n";
+    echo $mysqli->host_info . "\n" ;
+
+    $res = $mysqli->query("SELECT * FROM trickerlist WHERE tiebaid like '%KI%'");
+    $row = $res->fetch_assoc();
+    foreach($row as $keys=>$value){
+        echo $keys .' ' . $value . "<br>";
+    }
+
 ?>
