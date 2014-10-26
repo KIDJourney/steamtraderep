ade851cfc8bdfad2f139520aca81169558352c1e
Lonesome George <kingdeadfish@qq.com>
2014-10-27 01:01:42 +0800

    分离php与html，整合php代码

--- adminlogin.php
+++ adminlogin.php
@@ -4,7 +4,26 @@ session_start();
 <head>
     <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
 </head>
+
+
 <?php
-include("class/adminloginclass.php");
-$loginprocess = new login();
+
+if (!isset($_SESSION["admin"])){
+    include("template/adminlogin.html");
+    include("class/adminloginclass.php");
+    if ($_SERVER["REQUEST_METHOD"] == "POST")
+        $loginprocess = new login();
+} else {
+    relocation();
+}
+
+function relocation()
+{
+    sleep(1);
+    $Jscript = <<<JS
+    <script language="JavaScript"> self.location='managepage.php'; </script>
+JS;
+    echo $Jscript ;
+}
+
 ?>
