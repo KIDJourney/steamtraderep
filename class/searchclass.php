9f56e3c9cff25a9613e66564b6587f7461433d8a
Lonesome George <kingdeadfish@qq.com>
2014-10-27 09:40:38 +0800

    fix #4

--- class/searchclass.php
+++ class/searchclass.php
@@ -8,20 +8,22 @@ class search
 //public
     public function __construct()
     {
-            $this->userInput = $this->Fliter($_POST["userinput"]);
-            $this->sqlQuery = "SELECT * FROM trickerlist where 
-                     tiebaid like ? or 
-                     steamid like ? or 
-                     64weiid like ? or
-                     taobaoid like ? or
-                     zhifubaomail like ? or
-                     zhifubaoid like ?";
-            $this->sqlserach($this->user);
+        $this->userInput = "%".$_POST["userinput"]."%";
+        if (empty($this->userInput)){
+            die("请勿输入空白信息!");
+        }
+        $this->sqlQuery = "SELECT * FROM trickerlist where 
+                 tiebaid like ? or 
+                 steamid like ? or 
+                 64weiid like ? or
+                 taobaoid like ? or
+                 zhifubaomail like ? or
+                 zhifubaoid like ?";
+        $this->sqlserach();
     }
 
-    public function sqlserach($userInput)
+    public function sqlserach()
     {
-        $userInput = "%" . $userInput . "%";
         $mysqli = new mysqli(SAE_MYSQL_HOST_M.":".SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS,SAE_MYSQL_DB);
         if ($mysqli->connect_errno){
             die("服务器连接失败！请稍候重试！.<br>");
@@ -31,7 +33,7 @@ class search
                 echo "prepare error";
             } else {
                 $pQresult = array('s1','s2','s3','s4','s5','s6','s7','s8');
-                $pQProcess->bind_param("ssssss",$userInput,$userInput,$userInput,$userInput,$userInput,$userInput);
+                $pQProcess->bind_param("ssssss",$this->userInput,$this->userInput,$this->userInput,$this->userInput,$this->userInput,$this->userInput);
                 $pQProcess->execute();
                 $pQProcess->bind_result($pQresult[0], $pQresult[1], $pQresult[2], $pQresult[3], $pQresult[4], $pQresult[5], $pQresult[6], $pQresult[7]);
                 while($pQProcess->fetch()){
