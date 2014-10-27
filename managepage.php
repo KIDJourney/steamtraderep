c849ba5b41b0e1c5df3d5624a017565b09cf604b
Lonesome George <kingdeadfish@qq.com>
2014-10-27 16:34:55 +0800

    add back button

--- managepage.php
+++ managepage.php
@@ -6,12 +6,21 @@ session_start();
 </head>
 <?php
 if (!isset($_SESSION["admin"])){
-    die("你无权访问这个页面! . (from page) <br>");
+    die("你无权访问这个页面! .");
+    sleep(2);
+    relocation();
 } else {
     include("template/managepage.html");
     include("class/manageclass.php");
 }
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
     $manageprocess = new manage;
+}
+
+function relocation()
+{
+    $js = <<< js
+ <script language="JavaScript"> self.location='index.html'; </script>
+js;
 }
 ?>
