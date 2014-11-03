ea6d9f12a659b3ea9bc32ce035e6a3ac302d73c2
Lonesome George <kingdeadfish@qq.com>
2014-11-04 00:05:58 +0800

    fix and test

---
+++ report.php
@@ -0,0 +1,3 @@
+<?php
+include("template/report.html");
+?>
--- testpage.php
+++ testpage.php
@@ -1,15 +1,6 @@
 <?php
 
-$test = array("result"=>array(array("username"=>1,"name"=>"sb"),
-                              array("testname"=>2,"name"=>"sb2"),
-                              array("testinfo"=>3,"name"=>"sb3")));
-echo json_encode($test);
-
-$test = array("testarray"=>array());
-for ($i = 0 ; $i < 10 ; $i ++){
-    array_push($test["testarray"],array($i));
-}
-print_r($test);
+include("template/test.html");
 
 
 ?>
