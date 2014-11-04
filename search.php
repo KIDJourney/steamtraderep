6387f02463e75061449246b0e0fed009f8288db9
王同和 <Hetong583@qq.com>
2014-11-04 21:06:16 +0800

    删除了开发过程文件

--- search.php
+++ search.php
@@ -2,13 +2,8 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
     <link rel="stylesheet" type="text/css" href="css/base.css">
-    <link rel="stylesheet/less" type="text/css" href="css/search.less">
-    <script type="text/javascript">
-        var config = {"result":[{"tiebaid":" \u6b64ID\u4ee5\u6709\u4eba\u4f7f\u7528 oO____\u840c\u4e3b 13970012773 \u6570\u5b57\u541bwindy 996543061","steamid":"","64weiid":" 76561198076721482","taobaoid":"","zhifubaomail":" woaini7525057@163.com 275867993@qq.com 2359575017@qq.com","zhifubaoid":" \u987e\u5411\u6daa","":" \u6b3a\u8bc8"}],"status":1};
-    </script>
+    <link rel="stylesheet" type="text/css" href="css/search.css">
     <script type="text/javascript" src="js/jquery-2.1.0.min.js"></script>
-    <script type="text/javascript" src="js/less.min.js"></script>
-    <script type="text/javascript" src="js/search.js"></script>
 </head>
 <body>
     <div id="container">
@@ -19,6 +14,7 @@
         </form>
         <ul id="info"></ul>
     </div>
+    <script type="text/javascript" src="js/search.js"></script>
 </body>
 </html>
 
