d45c39be0e6410b8ebb90cb1c66675831e5f3622
王同和 <Hetong583@qq.com>
2014-11-25 00:11:09 +0800

    modify donator html

--- donatorinfo.php
+++ donatorinfo.php
@@ -1,10 +1,13 @@
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+    <script type="text/javascript" src="js/jquery-2.1.0.min.js"></script>
+
 </head>
-
-<?php
-
-include("class/donatorinfo.php");
-
-?>
+<body>
+<?php include("template/header.html"); ?>
+<div id="container">
+	<div id="title"></div>
+	<ul id="donators"></ul>
+</div>
+</body>
