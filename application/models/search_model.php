9b454ecefecbbc5bb2261fb1430a63e2be96f3aa
KIDJourney <kingdeadfish@qq.com>
2015-05-31 10:33:58 +0800

    infine css

--- application/models/search_model.php
+++ application/models/search_model.php
@@ -8,7 +8,7 @@
         }
 
         public function getsearch($userinput)
-        {  
+        {
             $query = array("tiebaid"=>$userinput,"steamid"=>$userinput,"idwei64"=>$userinput,"taobaoid"=>$userinput,"zhifubaomail"=>$userinput,"zhifubaoid"=>$userinput);
             $this->db->or_like($query);
             $result = $this->db->get("trickerlist");
