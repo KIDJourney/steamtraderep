7748efc338d0381bcc38c46156cfab8f2a3f181a
KIDJourney <kingdeadfish@qq.com>
2015-07-26 18:52:03 -0400

    reset to stable version

---
+++ application/views/search/donatorinfo.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <script type="text/javascript" src="<?php echo base_url('js/donatorinfo.js');?>"></script>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/base.css');?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/donatorinfo.css');?>">
+</head>
+<body>
+<div id="container">
+	<h2 id="title">Thanks For Your Generous Donation</h2>
+	<ul id="donators"></ul>
+</div>
+</body>
